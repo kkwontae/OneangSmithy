82d228ab04d38deb85f0c405150dbd74ca7885a0
kkwontae <2wsdfghjkl@gmail.com>
2022-06-14 18:03:01 +0900

    First Commit

---
+++ Source/OneangSmithy/Startup.cs
@@ -0,0 +1,105 @@
+using OneangSmithy_Server.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+
+namespace OneangSmithy_Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.IsEssential = true;
+            });
+
+            services.AddSession(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.None;
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.IsEssential = true;
+            });
+
+            //services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                //.AddCookie();
+            services.AddAuthentication().AddKakaoTalk(options =>
+            {
+                //options.ClientId = Configuration["KakaoTalk:ClientId"];
+                //options.ClientSecret = Configuration["KakaoTalk:ClientSecret"];
+                options.ClientId = "1f956f9c013c3e7f9963ab587b3f4994";
+                options.ClientSecret = "lmxYfea75ReNKDL6cnrV2wYsbfVW0b57";
+                options.CallbackPath = "/Kakao/login-callback";
+            });
+            // From: https://github.com/aspnet/Blazor/issues/1554
+            // Adds HttpContextAccessor
+            // Used to determine if a user is logged in
+            // and what their username is
+            services.AddHttpContextAccessor();
+            services.AddScoped<HttpContextAccessor>();
+            // Required for HttpClient support in the Blazor Client project
+            services.AddHttpClient();
+            services.AddScoped<HttpClient>();
+            // Pass settings to other components
+            services.AddSingleton<IConfiguration>(Configuration);
+
+
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
